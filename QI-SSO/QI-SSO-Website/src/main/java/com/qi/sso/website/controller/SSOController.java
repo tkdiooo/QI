6fbccb615b7510a3ba617e45c6815806a52eb444
tkdiooo <zhangqi_82@163.com>
2017-08-21 18:07:42 +0800

    commit

--- QI-SSO/QI-SSO-Website/src/main/java/com/qi/sso/website/controller/SSOController.java
+++ QI-SSO/QI-SSO-Website/src/main/java/com/qi/sso/website/controller/SSOController.java
@@ -1,6 +1,17 @@
 package com.qi.sso.website.controller;
 
 import com.qi.sso.common.constants.SSOConstants;
+import com.sfsctech.base.exception.VerifyException;
+import com.sfsctech.base.result.ValidatorResult;
+import com.sfsctech.common.security.rsa.KeyPairModel;
+import com.sfsctech.common.security.rsa.RSA;
+import com.sfsctech.common.util.HexUtil;
+import com.sfsctech.common.util.StringUtil;
+import com.sfsctech.constants.I18NConstants;
+import com.sfsctech.constants.LabelConstants;
+import com.sfsctech.rpc.result.ActionResult;
+import com.sfsctech.rpc.util.ValidatorUtil;
+import com.sfsctech.security.session.UserAuthData;
 import com.sfsctech.spring.properties.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Class SSOController
@@ -24,7 +40,7 @@ public class SSOController {
     private AppConfig appConfig;
 
     @GetMapping("index")
-    public String index(ModelMap model) {
+    public String index(ModelMap model, HttpServletRequest request) {
         // 注册
         model.put(SSOConstants.REGISTER_URL, appConfig.SSO_REGISTER_URL);
         // 找回密码
@@ -35,6 +51,60 @@ public class SSOController {
         model.put(SSOConstants.LOGOUT_URL, appConfig.SSO_LOGOUT_URL);
         // title
         model.put("title", "登录");
+        // RSA加密公钥生成
+        KeyPairModel keyPair;
+        if (null != RSA.LOCAL_KEYPAIRMODEL.get(request.getSession().getId())) {
+            keyPair = RSA.LOCAL_KEYPAIRMODEL.get(request.getSession().getId());
+        } else {
+            keyPair = RSA.getKeys();
+            RSA.LOCAL_KEYPAIRMODEL.put(request.getSession().getId(), keyPair);
+        }
+        String Modulus = keyPair.getPublicKey().getModulus().toString(16);
+        String Exponent = keyPair.getPublicKey().getPublicExponent().toString(16);
+        model.put("Digits", Modulus.length());
+        model.put("Modulus", Modulus);
+        model.put("Exponent", Exponent);
         return "index";
+    }
+
+    @PostMapping("login")
+    @ResponseBody
+    public ActionResult<UserAuthData> login(HttpServletRequest request, HttpServletResponse response) {
+        String account = request.getParameter(SSOConstants.LOGIN_ACCOUNT);
+        String password = request.getParameter(SSOConstants.LOGIN_PASSWORD);
+        ActionResult<UserAuthData> result = new ActionResult<>();
+        // 用户名或密码为空
+        if (StringUtil.isBlank(account) || StringUtil.isBlank(password)) {
+            result.setSuccess(false);
+            result.addMessage(I18NConstants.Tips.LoginAuthNotEmpty);
+            return result;
+        }
+        UserAuthData authData = new UserAuthData(loginDecrypt(request, account), loginDecrypt(request, password));
+        ValidatorResult valid = ValidatorUtil.validate(authData);
+        if (valid.hasErrors()) {
+            logger.error("数据校验异常：" + result.getMessages());
+            throw new VerifyException(I18NConstants.Tips.ExceptionValidator, valid);
+        }
+        String remember = request.getParameter(SSOConstants.LOGIN_REMEMBER);
+        String form_url = request.getParameter(SSOConstants.PARAM_FROM_URL);
+        // 验证登录信息，返回用户对象
+        boolean bool = true;
+//        result.setSuccess(false);
+//        result.addMessage(I18NConstants.Tips.LoginWrong);
+        return result;
+    }
+
+
+    public static String loginDecrypt(HttpServletRequest request, String result) {
+        KeyPairModel keyPair = RSA.LOCAL_KEYPAIRMODEL.get(request.getSession().getId());
+        if (null != keyPair) {
+            byte[] result_byte = RSA.simpleDecrypt(keyPair.getPrivateKey(), HexUtil.hexStringToBytes(result));
+            if (null != result_byte) {
+                StringBuilder sb = new StringBuilder(new String(result_byte));
+                return sb.reverse().toString();
+            }
+            RSA.LOCAL_KEYPAIRMODEL.remove(request.getSession().getId());
+        }
+        return "";
     }
 }
