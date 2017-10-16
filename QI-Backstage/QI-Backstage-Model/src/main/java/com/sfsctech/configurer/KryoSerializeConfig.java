6af6b7074a16711115bf25794821d587881f59bd
tkdiooo <zhangqi_82@163.com>
2017-10-16 22:27:33 +0800

    commit

---
+++ QI-Backstage/QI-Backstage-Model/src/main/java/com/sfsctech/configurer/KryoSerializeConfig.java
@@ -0,0 +1,18 @@
+package com.sfsctech.configurer;
+
+import com.sfsctech.constants.DubboConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class InitConfig
+ *
+ * @author 张麒 2017/5/26.
+ * @version Description:
+ */
+@Component("BACKSTAGE_MODEL_SERIALIZE")
+public class KryoSerializeConfig {
+
+    static {
+        DubboConstants.addKryoSerializePackage("com.qi.management.model.dto");
+    }
+}
