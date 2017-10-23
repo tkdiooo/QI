b86cf2eae16ba874cdfa2c95b15a7c35c79f3524
tkdiooo <zhangqi_82@163.com>
2017-10-23 16:59:19 +0800

    commit

---
+++ QI-Platform/QI-Platform-Backend/src/main/java/com/qi/platform/backend/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.qi.platform.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Class IndexController
+ *
+ * @author 张麒 2017/10/17.
+ * @version Description:
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("index")
+    public String index(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
+        return "index";
+    }
+}
